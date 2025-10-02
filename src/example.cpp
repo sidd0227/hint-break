c682c8be44d223ac9eb7da7903c28b0e720984c0
colby57 <67706172+colby57@users.noreply.github.com>
2025-10-02 07:07:54 +0300

    added example & binary

---
+++ src/example.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <array>
+#include <windows.h>
+
+int main () {
+    std::array <uint8_t, 11> shell = { 0x0F, 0x1A, 0x24, 0x4C, 0x30, 0xC0, 0x0F, 0x1B, 0x24, 0x4C, 0xC3 };
+
+    auto mem = VirtualAlloc(0, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    std::memcpy(mem, shell.data(), shell.size());
+
+    using fn = int (__fastcall*)();
+    fn func = reinterpret_cast<fn>(mem);
+    func ();
+
+    return 0;
+}
